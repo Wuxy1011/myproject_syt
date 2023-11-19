a90396b7f99235ec1e34a80d7dbd0184de0e5b69
wuxy <1660630433@qq.com>
2023-11-19 20:09:36 +0800

    11

--- src/components/hospital_top/index.vue
+++ src/components/hospital_top/index.vue
@@ -2,7 +2,16 @@
 import { reduceEachLeadingCommentRange } from 'typescript';
 <template>
     <div class="top">
-        <div class="content"></div>
+        <div class="content">
+			<div class="left">
+				<img src="../../assets/images/logo.png" alt="">
+				<p>尚医通 预约挂号统一平台</p>
+			</div>
+			<div class="right">
+				<p class="help">帮助中心</p>
+				<p class="login">登录/注册</p>
+			</div>
+		</div>
     </div>
 </template>
 
@@ -22,6 +31,12 @@ import { reduceEachLeadingCommentRange } from 'typescript';
         background: red;
         width: 80%;
         height: 70px;
+		display: flex;
+		justify-content: space-between;
+		img{
+			width: 50px;
+			height: 50px;
+		}
     }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import '@/style/reset.scss'
 import App from '@/App.vue'
 
 // 引入全局组件
-import HospitalTop from "@/components/Hospital_top/index.vue"
+import HospitalTop from "../src/components/hospital_top/index.vue"
 // 创建app实例，且将应用实例挂载
 const app=createApp(App)
 
