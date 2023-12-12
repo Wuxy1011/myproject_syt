8cf3954329668a331fba7435b6e6c48907493458
wuxy <1660630433@qq.com>
2023-12-12 17:32:05 +0800

    医院详情图标

--- src/components/hospital_top/index.vue
+++ src/components/hospital_top/index.vue
@@ -2,22 +2,27 @@
   <div class="top">
     <div class="content">
       <!-- 左侧 -->
-      <div class="left" >
+      <div class="left" @click="goHome">
         <img src="../../assets/images/logo.png" alt="" />
         <p>尚医通 预约挂号统一平台</p>
       </div>
       <div class="right">
         <p class="help">帮助中心</p>
         <!-- 如果没有用户名字:显示登录注册 -->
-        <p class="login" >登录/注册</p>
-        
+        <p class="login">登录/注册</p>
+
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { useRouter } from 'vue-router'
+let $router = useRouter()
+// 点击跳转到首页
+const goHome = () => {
+  $router.push({ path: '/home' })
+}
 </script>
 
 <style scoped lang="scss">
@@ -29,36 +34,41 @@
 
   display: flex;
   justify-content: center;
+
   .content {
     width: 1200px;
     height: 70px;
     background: white;
     display: flex;
     justify-content: space-between;
+
     .left {
       display: flex;
       justify-content: center;
       align-items: center;
+
       img {
         width: 50px;
         height: 50px;
         margin-right: 10px;
       }
+
       p {
         font-size: 20px;
         color: #55a6fe;
       }
     }
+
     .right {
       display: flex;
       align-items: center;
       justify-content: center;
       font-size: 14px;
       color: #bbb;
+
       .help {
         margin-right: 10px;
       }
     }
   }
-}
-</style>
+}</style>
--- src/pages/hospital/index.vue
+++ src/pages/hospital/index.vue
@@ -1,14 +1,55 @@
 <template>
   <div class="hospital">
-  <RouterView></RouterView>
-        
+    <!-- 左侧导航区域 -->
+    <div class="top">
+      <el-icon>
+        <HomeFilled />
+      </el-icon>
+      <span>医院信息</span>
+    </div>
+    <el-menu :default-active="$route.path" class="el-menu-vertical-demo">
+      <el-menu-item index="/hospital/register">
+        <el-icon><icon-menu /></el-icon>
+        <span>预约挂号</span>
+      </el-menu-item>
+      <el-menu-item index="/hospital/detail">
+        <el-icon>
+          <document />
+        </el-icon>
+        <span>医院详情</span>
+      </el-menu-item>
+      <el-menu-item index="/hospital/notice">
+        <el-icon>
+          <setting />
+        </el-icon>
+        <span>预约通知</span>
+      </el-menu-item>
+      <el-menu-item index="/hospital/close">
+        <el-icon>
+          <InfoFilled />
+        </el-icon>
+        <span>停诊信息</span>
+      </el-menu-item>
+      <el-menu-item index="/hospital/search">
+        <el-icon>
+          <Search />
+        </el-icon>
+        <span>查询/取消</span>
+      </el-menu-item>
+    </el-menu>
+    <RouterView></RouterView>
+
   </div>
 </template>
 
 <script setup lang="ts">
-
+import {
+  HomeFilled, Document,
+  Menu as IconMenu,
+  Setting,
+  InfoFilled,
+  Search,
+} from '@element-plus/icons-vue/global';
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
